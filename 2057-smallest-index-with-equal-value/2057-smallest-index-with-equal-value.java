d831fd740fcb38ab3d5c6a1f27fc3fbc9fd6317e
Nitish Yadav <nitishyadav1817@gmail.com>
2026-09-20 21:40:01 +0530

    Time: 0 ms (100%), Space: 46.4 MB (40.78%) - LeetHub

--- 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.java
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.java
@@ -1,13 +1,14 @@
-class Solution {
-    public int smallestEqual(int[] nums) {
-        int min = Integer.MAX_VALUE;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == i%10){
-                min = Math.min(i, min);
+class Solution
+{
+    public int smallestEqual(int[] nums)
+    {
+        for(int i=0;i< nums.length;i++)
+        {
+            if(i%10== nums[i])
+            {
+                return i;
             }
         }
-        if(min == Integer.MAX_VALUE) return -1;
-        else
-        return min;
+        return -1;
     }
 }
